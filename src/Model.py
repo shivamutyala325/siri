223a080b8eccb69a7b485f5290f6643145b79b32
shiva mutyala <shivamutyala325@gmail.com>
2025-11-25 22:15:48 +0530

    Build Model with google genai api

---
+++ src/Model.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from google import genai
+from google.genai import types
+
+class useModel():
+    def __init__(self):
+        self.model='gemini-2.5-flash'
+        self.prompt="You are a model for extracting the content from the invoice and return the list of items with quantity and price of each item"
+        
+
+    def generate(self):
+        with open("../data/image1.png", 'rb') as f:
+            image_bytes = f.read()
+
+        client = genai.Client()
+        response = client.models.generate_content(
+            model=self.model,
+            contents=[
+            self.prompt,
+            types.Part.from_bytes(
+                data=image_bytes,
+                mime_type='image/png',
+            ),
+            'Caption this image.'
+            ]
+        )
+
+        print(response.text)
+
+model=useModel()
+model.generate()
